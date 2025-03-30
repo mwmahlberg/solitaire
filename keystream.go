6707473c1747d47aa66ed48d6ec6b49b06df8a39
Markus W. Mahlberg <markus@mahlberg.io>
2025-03-31 00:54:41 +0200

    style: further cleanup

--- keystream.go
+++ keystream.go
@@ -21,7 +21,7 @@ const (
 	jokerB card = 54
 )
 
-var alphabet = []byte{
+var alphabet = [26]byte{
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 	'U', 'V', 'W', 'X', 'Y', 'Z'}
